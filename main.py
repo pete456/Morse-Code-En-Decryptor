3b9732547d1f96f2e7fdb8a48fc1c41e70ea7796
Peter McMaster <pmcmaster9090@gmail.com>
2019-02-22 10:10:12 -0500

    Completed convertoenglish function

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ class NotFound(Exception):
 class NoArgValue(Exception):
     pass
 
+helpstring ="""python main.py [options] [type] file
+[options]
+-h: prints this chunck of text.
+-f: Uses custom file for morse code. Must be formatted with char,code. Defaults to morsesheet.csv.
+-w: Character used to signify a space between words. Defaults to |.
+-c: Character used to signify a new character. Defaults to a space.
+
+[type]
+-m: Converts Morsecode to English.
+-e: Converts English to Morse.
+"""
+
 class Node:
     def __init__(self, parent, index, key, value):
         self.parent = parent
@@ -89,42 +101,57 @@ def getsortedmorselist(file):
     morselist = sortlist(morselist)
 
     if morselist[0][0] == '':
-        morselist[0]=(' ',' ')
+        del morselist[0]
 
     return morselist
 
-def getmorsetree(file):
+def createmorsetree(file):
+    """Calls getsortedmorselist in order to get a list of (Morse code,English letter) tuples. Each tuple is added as a node to a BST object. new BST object is returned after. 
+    """
     file = open(file)
     morselist = getsortedmorselist(file)
     file.close()
     morsetree = BTS()
-    for i in range(1,len(morselist)):
+    
+    for i in range(0,len(morselist)):
         morsetree.append(morselist[i][0],morselist[i][1])
         
     return morsetree;
 
+def morsetoword(list, tree):
+    """Converts a list of morse character to an English string.
+    """
+    englishword=[] #Characters are added to this list after they have been decrypted
+    for char in list:
+        if char != '':
+            englishword.append(tree.search(char))
+            
+    return ''.join(englishword)
+            
 def converttoenglish(cspace, wspace, file, morsesheet):
-    getmorsetree(file=morsesheet)
+    mbst=createmorsetree(file=morsesheet)
     englishstring=[]
+    
     file = open(file)
     for line in file:
         wordlist=line.split(wspace)
-        print(wordlist)
-
+        if '\n' in wordlist:
+            del wordlist[wordlist.index('\n')]
+        
+        englishsent=[]
+        for word in wordlist:
+            characterlist=word.split(cspace)
+            
+            englishsent.append(morsetoword(characterlist,mbst)) #Adds the word that was decrypted to a list containing all words in the current line.
+            englishsent.append(' ')
+            
+        englishstring.append(''.join(englishsent))
+        englishsent=[]
+        
+    return englishstring
+        
 def converttomorse(cspace, wspace, file, morsesheet):
     pass
-
-helpstring ="""python main.py [options] [type] file
-[options]
--h: prints this chunck of text.
--f: Uses custom file for morse code. Must be formatted with char,code. Defaults to morsesheet.csv.
--w: Character used to signify a space between words. Defaults to |.
--c: Character used to signify a new character. Defaults to a space.
-
-[type]
--m: Converts Morsecode to English.
--e: Converts English to Morse.
-"""
 
 def getargvalue(list,arg):
     argindex=list.index(arg)
@@ -135,12 +162,12 @@ def getargvalue(list,arg):
 def invalidarg():
     print('Invalid argument')
     exit()
-    
+
 if __name__ == '__main__':
     arglist=sys.argv[1:]
     morsesheet='morsesheet.csv'
     encodedtype=''
-    wspace='|'
+    wspace='/'
     cspace=' '
     
     validargs=['-f','-w','-c','-m','-e']
@@ -171,6 +198,8 @@ if __name__ == '__main__':
         cspace=argdict['-c']
         
     if '-m' in argdict:
-        converttoenglish(cspace=cspace,wspace=wspace, file=arglist[len(arglist)-1], morsesheet=morsesheet)
+        englishstring=converttoenglish(cspace=cspace,wspace=wspace, file=arglist[len(arglist)-1], morsesheet=morsesheet)
+        for sent in englishstring:
+            print(sent)
     else:
         converttomorse(cspace=cspace,wspace=wspace, file=arglist[len(arglist)-1], morsesheet=morsesheet)
